79954741d9df619b93fa375320f3fefb8e9d438b
egsweeny <esweeny@scu.edu>
2016-08-22 16:36:54 -0700

    Updated table in admin edit reservations.

--- hotelguis/adminEditReservations.java
+++ hotelguis/adminEditReservations.java
@@ -254,7 +254,15 @@ public class adminEditReservations extends javax.swing.JDialog {
             new String [] {
                 "Reservation ID", "User ID", "Start Date", "End Date"
             }
-        ));
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.Integer.class, java.lang.String.class, java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
         reservationDisplayTable.setColumnSelectionAllowed(true);
         jScrollPane1.setViewportView(reservationDisplayTable);
         reservationDisplayTable.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
