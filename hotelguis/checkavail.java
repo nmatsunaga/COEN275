b0dd91715c41eb710a86629960b00d4d28e0436f
egsweeny <esweeny@scu.edu>
2016-08-22 16:14:57 -0700

    minor adjustment

--- hotelguis/checkavail.java
+++ hotelguis/checkavail.java
@@ -365,8 +365,8 @@ public class checkavail extends javax.swing.JDialog {
 				}
 				else
 				{
+                    hotelsystemMAIN.reportError("You must log-in first to make a reservation.");                
                     this.dispose();
-                    hotelsystemMAIN.reportError("You must log-in first to make a reservation.");
                     loginpage loginwindow = new loginpage(new javax.swing.JFrame(), true);
                     loginwindow.setSize(800,620);
                     loginwindow.setVisible(true);
